0373fe31125385608b91dfa485a37c016d74651c
Galosoft <internick12@gmail.com>
2020-07-14 11:26:22 -0500

    Segundo commit

--- app/src/main/java/com/galosoft/pruebagit/MainActivity.kt
+++ app/src/main/java/com/galosoft/pruebagit/MainActivity.kt
@@ -6,7 +6,8 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        //super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
